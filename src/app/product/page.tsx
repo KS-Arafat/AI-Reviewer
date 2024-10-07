776797707ad40fd4a040452a13a9c771a60f3ef9
KS-Arafat <sfnarafat@gmail.com>
2024-10-07 17:45:06 +0600

    Product List Page Done

---
+++ src/app/product/page.tsx
@@ -0,0 +1,96 @@
+import Link from "next/link";
+import ProductCard from "./ProductCard";
+import { mistergrape } from "../fontProvider";
+
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+  image: string;
+}
+
+const products: Product[] = [
+  {
+    id: 2,
+    name: "Iphone",
+    price: 99.99,
+    description: "Over Priced Trash Phone",
+    image: "/products/product_02.webp",
+  },
+
+  {
+    id: 4,
+    name: "Tablet",
+    price: 99.99,
+    description: "Some brand of Ipad",
+    image: "/products/product_04.webp",
+  },
+  {
+    id: 5,
+    name: "Apple Watch",
+    price: 99.99,
+    description: "Electronic Waste",
+    image: "/products/product_05.webp",
+  },
+];
+
+export function ProductGrid() {
+  return (
+    <div className="">
+      <div className="grid h-20 min-w-full grid-cols-3 rounded-b-md bg-gradient-to-br from-sky-500 via-blue-300 to-cyan-300 shadow-lg shadow-sky-200">
+        <Link className="w-24 p-4" href="/">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="lucide lucide-house mx-auto h-12 w-12 transition-all ease-in-out hover:stroke-amber-300"
+          >
+            <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8" />
+            <path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+          </svg>
+        </Link>
+
+        <span
+          className={`w-5/6 pt-3 text-center text-6xl ${mistergrape.className} from-yellow-300 via-orange-400 to-amber-500 bg-clip-text transition-all hover:bg-gradient-to-br hover:text-transparent`}
+        >
+          Product List
+        </span>
+        <div className="flex flex-row items-end justify-end p-4">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="lucide lucide-user-round-search relative right-0 h-12 w-12"
+          >
+            <circle cx="10" cy="8" r="5" />
+            <path d="M2 21a8 8 0 0 1 10.434-7.62" />
+            <circle cx="18" cy="18" r="3" />
+            <path d="m22 22-1.9-1.9" />
+          </svg>
+        </div>
+      </div>
+      <div className="container mx-auto px-4 py-8">
+        <div className="flex select-none flex-row items-center justify-center gap-6">
+          {products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ProductGrid;
